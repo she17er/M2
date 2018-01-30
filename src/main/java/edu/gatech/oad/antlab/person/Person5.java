81f74bd93d1de3e82f3c015276160e27c2d5115e
Kevin Luikey <kluikey@gmail.com>
2018-01-30 15:46:47 -0500

    Finished Person5.java

--- src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -30,8 +30,7 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
+	  return name.substring(2) + name.substring(0, 2);
 	}
 	
 	/**
@@ -43,7 +42,7 @@ public class Person5 {
 	 *         object
 	 */
 	public String toString(String input) {
-	  return name.substring(2) + name.substring(0, 2);
+	  return name + calc(input);
 	}
 
 }
