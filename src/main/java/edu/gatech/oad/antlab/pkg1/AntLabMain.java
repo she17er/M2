f1d7ee3d696c60f3630e9465d36b5ebd06e161c4
Kaijun Lin <31287315+Klin04@users.noreply.github.com>
2018-01-31 15:34:14 -0500

    change kevin's gt account

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -74,7 +74,7 @@ import edu.gatech.oad.antlab.pkg2.*;
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
                   Person5 p5 = new Person5("Kevin Luikey");
-                  toPrint += p5.toString("gburdell5");
+                  toPrint += p5.toString("kluikey3");
 		  Person6 p6 = new Person6("Elissa");
 		  toPrint += p6.toString("ehuang39");
         System.out.println(toPrint);
